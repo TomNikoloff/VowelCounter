0798a3d60df8d5b2c5c3fbd593fd89babe2a1282
TomNikoloff <tomnikoloff@hotmail.co.uk>
2022-02-16 20:24:39 +0000

    Test

--- vowelCounter.js
+++ vowelCounter.js
@@ -1,8 +1,31 @@
 
-const input = document.querySelector("input");
+let input = document.querySelector("input").value;
 const checkBtn = document.querySelector(".checkBtn");
 const vowels = ['a', 'e', 'i', 'o', 'u'];
+let count = 0;
 
-function vowelCounter() {
-
+/*
+function inputLength() {
+    return input.value.length;
 }
+*/
+
+function vowelCounter(input) {
+    
+    for (let i = 0; i < input.length; i++) {
+        if(input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u') {
+            count++;
+        }
+    }
+
+    alert('There are ' + count + ' vowels');
+}
+
+function check() {
+    console.log('test');
+    if (input > 0) {
+        vowelCounter();
+    }
+}
+
+checkBtn.addEventListener('click', check);
